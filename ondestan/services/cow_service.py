f6ef266a1af696f1e5338e12a9a41f6dd5d6eace
Jorge López <jlopez@cartolab.es>
2014-03-17 21:19:58 +0100

    Added return statements
    They worked without them, but the explicit return
    makes reading the code easier.

--- ondestan/services/cow_service.py
+++ ondestan/services/cow_service.py
@@ -3,7 +3,7 @@ from ondestan.entities import Cow
 
 
 def get_cow_by_user_login(login):
-    Cow().queryObject().filter(Cow.user.has(login=login)).all()
+    return Cow().queryObject().filter(Cow.user.has(login=login)).all()
 
 
 def get_all_cows():
--- ondestan/services/plot_service.py
+++ ondestan/services/plot_service.py
@@ -3,7 +3,7 @@ from ondestan.entities import Plot
 
 
 def get_plot_by_user_login(login):
-    Plot().queryObject().filter(Plot.user.has(login=login)).all()
+    return Plot().queryObject().filter(Plot.user.has(login=login)).all()
 
 
 def get_all_plots():
