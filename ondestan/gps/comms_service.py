ddd1c32ece0675c9626edec6f635dea805612433
Jorge López <jlopez@cartolab.es>
2014-03-20 15:34:14 +0100

    Post petition example improved.

--- ondestan/gps/comms_service.py
+++ ondestan/gps/comms_service.py
@@ -3,17 +3,22 @@ import logging
 
 logger = logging.getLogger('ondestan')
 
+
 def process_data_updates(params):
     if ('mac' in params):
         mac = str(params['mac'])
-        process_data_update(mac)
+        battery = str(params['battery'])
+        process_data_update(mac, battery)
     elif ('mac[0]' in params):
         i = 0
         while ('mac[' + str(i) + ']' in params):
             mac = str(params['mac[' + str(i) + ']'])
-            process_data_update(mac)
+            battery = str(params['battery[' + str(i) + ']'])
+            process_data_update(mac, battery)
             i += 1
     return True
 
-def process_data_update(mac):
-    logger.info('Processing update for mac: ' + mac)
+
+def process_data_update(mac, battery):
+    logger.info('Processing update for mac: ' + mac +
+                ' with battery level ' + battery)
--- scripts/post.py
+++ scripts/post.py
@@ -2,9 +2,16 @@ import httplib
 import urllib
 
 
-params = urllib.urlencode({'mac[0]': '11111111'})
+params = urllib.urlencode({'mac': '11111111', 'battery': '80'})
 headers = {"Content-type": "application/x-www-form-urlencoded",
             "Accept": "text/plain"}
+conn = httplib.HTTPConnection("127.0.0.1", 6543)
+conn.request("POST", "/gps_update", params, headers)
+response = conn.getresponse()
+print response.status, response.reason
+
+params = urllib.urlencode({'mac[0]': '11111111', 'mac[1]': '22222222',
+                           'battery[0]': '80', 'battery[1]': '15'})
 conn = httplib.HTTPConnection("127.0.0.1", 6543)
 conn.request("POST", "/gps_update", params, headers)
 response = conn.getresponse()
