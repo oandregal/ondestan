e0c52b8bba4b1dc41bd94693503a3fb92da50237
Jorge López <jlopez@cartolab.es>
2014-03-21 13:40:14 +0100

    Changed form validation to bootstrap+jqvalidate

--- ondestan/static/js/signup.js
+++ ondestan/static/js/signup.js
@@ -1,21 +1,29 @@
-function checkFields() {
-	login = document.getElementById('login').value;
-	name = document.getElementById('name').value;
-	email = document.getElementById('email').value;
-	pass1 = document.getElementById('password1').value;
-	pass2 = document.getElementById('password2').value;
-	document.getElementById('submit').disabled = ((pass1 != pass2) || (pass1 == '') || (login == '') || (name == '') || (email == ''));
-	if (pass1 != '') {
-	if (pass1.length < 6) {
-		document.getElementById('passtooshort').style.display="";
-		document.getElementById('passdistinct').style.display="none";
-	} else {
-		document.getElementById('passtooshort').style.display="none";
-	if ((pass2 != '') && (pass1 != pass2)) {
-		document.getElementById('passdistinct').style.display="";
-	} else {
-		document.getElementById('passdistinct').style.display="none";
-	}
-	}
-	}
-}
+$.extend($.validator.messages, {
+    required: window.contextVariables.required_field_msg,
+    minlength: jQuery.validator.format(window.contextVariables.minlength_field_msg),
+    email: window.contextVariables.email_field_msg
+});
+$(function() {
+	$('#form').validate({
+		rules : {
+			login : "required",
+			name : "required",
+			email : {
+				email : true,
+				required : true
+			},
+			password : {
+				required: true,
+				minlength: 6
+			},
+			password2 : {
+				equalTo: "#password"
+			}
+		},
+	    messages: {
+	        password2: {
+	        	equalTo: window.contextVariables.passwords_dont_match_msg
+	        }
+	    }
+	});
+});
