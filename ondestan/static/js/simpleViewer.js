2a4cf805711ca60751293090ff60253649dfb87e
Jorge López <jlopez@cartolab.es>
2014-03-21 18:48:26 +0100

    Fill the dashboard numbers with the real data.

--- ondestan/static/js/simpleViewer.js
+++ ondestan/static/js/simpleViewer.js
@@ -1,13 +1,17 @@
 $( function() {
 	var map = L.map('map').setView([51.505, -0.09], 13);
+	var active_devices = [];
+	var low_battery_devices = [];
 
 	function load_cows() {
-		new L.GeoJSON.AJAX(contextVariables.application_url + "/json/cows.json",{
+		layer = new L.GeoJSON.AJAX(contextVariables.application_url + "/json/cows.json",{
 			pointToLayer: function (feature, latlng) { 
 				var color = "green"; 
-				var weight = 0; 
+				var weight = 0;
+				active_devices.push(feature);
 	            if (feature.properties.battery_level < 20.0) {
 	            	color = "red";
+	            	low_battery_devices.push(feature);
 	            } else {
 		            if (feature.properties.battery_level < 50.0) {
 		            	color = "yellow";
@@ -28,7 +32,12 @@ $( function() {
             onEachFeature: function (feature, layer) {
                 layer.bindPopup(feature.properties.popup);
 	        }
-		}).addTo(map);
+		});
+		layer.on('data:loaded', function(e) {
+			$('#low_battery_devices').text(low_battery_devices.length);
+			$('#active_devices').text(active_devices.length);
+		});
+		layer.addTo(map);
 	}
 
 	new L.GeoJSON.AJAX(contextVariables.application_url + "/json/plots.json",{
