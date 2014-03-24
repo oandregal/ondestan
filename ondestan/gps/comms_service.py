86177e5d31bbf508ee0a6219697fd01df6ad7ff5
Jorge López <jlopez@cartolab.es>
2014-03-24 20:30:48 +0100

    Added more fields to the post gps update.

--- ondestan/gps/comms_service.py
+++ ondestan/gps/comms_service.py
@@ -1,9 +1,11 @@
 # coding=UTF-8
 import logging
 import md5
+from datetime import datetime
 from gps_update_error import GpsUpdateError
 
 logger = logging.getLogger('ondestan')
+date_format = '%Y-%m-%dT%H:%M:%S'
 
 
 def process_data_updates(request):
@@ -14,20 +16,40 @@ def process_data_updates(request):
     expected_md5 = request._headers['content-md5']
     if expected_md5 != md5.new(request.body).hexdigest():
         raise GpsUpdateError('Wrong MD5')
-    params = request.params
-    if ('mac' in params):
+    process_data(request.params)
+
+
+def process_data(params):
+    if 'mac' in params and 'password' in params and 'date' in params:
         mac = str(params['mac'])
-        battery = str(params['battery'])
-        process_data_update(mac, battery)
-    elif ('mac[0]' in params):
+        date = datetime.strptime(str(params['date']), date_format)
+        battery = float(params['battery'])
+        coverage = float(params['coverage'])
+        latitude = float(params['latitude'])
+        longitude = float(params['longitude'])
+        logger.info('Processing update for mac: ' + mac +
+                ' for date ' + date.strftime(date_format) +
+                ' with battery level ' + str(battery) +
+                ' with coverage ' + str(coverage) +
+                ' at latitude ' + str(latitude) +
+                ' and longitude ' + str(longitude))
+    elif 'mac[0]' in params and 'password[0]' in params and\
+        'date[0]' in params:
         i = 0
         while ('mac[' + str(i) + ']' in params):
             mac = str(params['mac[' + str(i) + ']'])
-            battery = str(params['battery[' + str(i) + ']'])
-            process_data_update(mac, battery)
+            date = datetime.strptime(str(params['date[' + str(i) + ']']),
+                                     date_format)
+            battery = float(params['battery[' + str(i) + ']'])
+            coverage = float(params['coverage[' + str(i) + ']'])
+            latitude = float(params['latitude[' + str(i) + ']'])
+            longitude = float(params['longitude[' + str(i) + ']'])
+            logger.info('Processing update for mac: ' + mac +
+                ' for date ' + date.strftime(date_format) +
+                ' with battery level ' + str(battery) +
+                ' with coverage ' + str(coverage) +
+                ' at latitude ' + str(latitude) +
+                ' and longitude ' + str(longitude))
             i += 1
-
-
-def process_data_update(mac, battery):
-    logger.info('Processing update for mac: ' + mac +
-                ' with battery level ' + battery)
+    else:
+        raise GpsUpdateError('Insufficient params')
--- scripts/post.py
+++ scripts/post.py
@@ -3,15 +3,20 @@ import md5
 
 s = Session()
 url = "http://0.0.0.0:6543/gps_update"
-params = {'mac': '11111111', 'battery': 80}
+params = {'mac': '11111111', 'password': '123456',
+          'date': '2013-05-14T19:04:00', 'battery': 80, 'coverage': 80,
+          'latitude': 91.12, 'longitude': 23.23}
 req = Request('POST', url, data=params)
 prepped = req.prepare()
-prepped.headers['Content-MD5'] = md5.new(prepped.body).hexdigest() + 'a'
+prepped.headers['Content-MD5'] = md5.new(prepped.body).hexdigest()
 r = s.send(prepped)
 print str(r.status_code) + " " + r.reason
 
-params = {'mac[0]': '11111111', 'mac[1]': '22222222',
-          'battery[0]': 80, 'battery[1]': 15}
+params = {'mac[0]': '11111111', 'mac[1]': '22222222', 'password[0]': '123456',
+          'password[1]': '123456', 'date[0]': '2013-05-14T19:04:00',
+          'date[1]': '2013-05-14T19:04:00', 'battery[0]': 80, 'battery[1]': 15,
+          'coverage[0]': 80, 'coverage[1]': 15, 'latitude[0]': 91.12,
+          'latitude[1]': 23.23, 'longitude[0]': 23.23, 'longitude[1]': 91.12}
 req = Request('POST', url, data=params)
 prepped = req.prepare()
 prepped.headers['Content-MD5'] = md5.new(prepped.body).hexdigest()
