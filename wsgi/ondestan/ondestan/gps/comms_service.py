8d6febaccbe714eb7ab71a22bdff13609b850bec
Jorge López <jlopez@cartolab.es>
2014-04-10 19:27:19 +0200

    Fixed a mistake...

--- wsgi/ondestan/ondestan/gps/comms_service.py
+++ wsgi/ondestan/ondestan/gps/comms_service.py
@@ -40,14 +40,14 @@ def process_gps_updates(request):
 
 def process_gps_params(params):
     positions = params.split(positions_divider)
-    # Temporarily return OK if the petition has too many params
-    if len(positions) > len(params_positions):
-        logger.warning('Received a GPS update with too many params...')
-        return
     for position in positions:
         params = position.split(params_divider)
         i = 0
         data = base_data.copy()
+        # Temporarily return OK if the petition has too many params
+        if len(params) > len(params_positions):
+            logger.warning('Received a GPS update with too many params...')
+            continue
         if (len(params) != len(params_positions)):
             raise GpsUpdateError('Insufficient POST params', 400)
         for key in params_positions:
