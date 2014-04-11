5772cf01670c38835aee5a774a996402fe644c80
Jorge López <jlopez@cartolab.es>
2014-04-11 14:39:00 +0200

    Added a utility method for creating a localizer
    Now we can create a localizer by a locale code.

--- wsgi/ondestan/ondestan/utils/__init__.py
+++ wsgi/ondestan/ondestan/utils/__init__.py
@@ -1,6 +1,6 @@
 from ondestan.utils.config import Config
-from ondestan.utils.comms import send_mail
+from ondestan.utils.comms import send_mail, send_sms
 from ondestan.utils.singleton import Singleton
 from ondestan.utils.db import Db, Base
 from ondestan.utils.html_container import HtmlContainer
-from ondestan.utils.various import rand_string
+from ondestan.utils.various import rand_string, get_custom_localizer
--- wsgi/ondestan/ondestan/utils/various.py
+++ wsgi/ondestan/ondestan/utils/various.py
@@ -2,9 +2,35 @@ from os import urandom
 from itertools import islice, imap, repeat
 import string
 
+from pyramid.interfaces import (
+    ILocalizer,
+    ITranslationDirectories
+    )
+from pyramid.i18n import (
+    get_current_registry,
+    make_localizer
+    )
+
 
 def rand_string(length=10):
     chars = set(string.ascii_uppercase + string.ascii_lowercase
                 + string.digits)
     char_gen = (c for c in imap(urandom, repeat(1)) if c in chars)
     return ''.join(islice(char_gen, None, length))
+
+
+def get_custom_localizer(locale):
+    if locale == None:
+        locale = ''
+
+    registry = get_current_registry()
+    localizer = registry.queryUtility(ILocalizer, name=locale)
+
+    if localizer is None:
+        # no localizer utility registered yet
+        tdirs = registry.queryUtility(ITranslationDirectories, default=[])
+        localizer = make_localizer(locale, tdirs)
+
+        registry.registerUtility(localizer, ILocalizer,
+                                 name=locale)
+    return localizer
