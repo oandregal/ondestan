402e16e821f7d7a735ce759ca7257a2d6c97ecf3
Jorge López <jlopez@cartolab.es>
2014-04-11 16:13:46 +0200

    Changed an http error code

--- wsgi/ondestan/ondestan/gps/comms_service.py
+++ wsgi/ondestan/ondestan/gps/comms_service.py
@@ -69,7 +69,7 @@ def process_gps_params(base_params):
                 i += 1
             process_gps_data(data)
     else:
-        raise GpsUpdateError('Unaccepted data', 403)
+        raise GpsUpdateError('Unaccepted data', 400)
 
 def process_gps_data(data):
     try:
