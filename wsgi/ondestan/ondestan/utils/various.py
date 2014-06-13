fa59d79c2a3dba1fc29478a036276dd5bdeeccbc
Jorge López <jlopez@cartolab.es>
2014-06-13 14:40:46 +0200

    Added a function for getting dates in a fancy format
    They are returned as a time difference (e.g. "3
    years and 2 months ago").
    Also refactored other functions.

--- wsgi/ondestan/ondestan/utils/various.py
+++ wsgi/ondestan/ondestan/utils/various.py
@@ -17,6 +17,21 @@ from itertools import islice, imap, repeat
 import string
 from datetime import datetime
 from dateutil import tz
+from dateutil.relativedelta import relativedelta
+
+# We put these 18n strings here so they're detected when parsing files
+_('year', domain='Ondestan')
+_('years', domain='Ondestan')
+_('month', domain='Ondestan')
+_('months', domain='Ondestan')
+_('day', domain='Ondestan')
+_('days', domain='Ondestan')
+_('hour', domain='Ondestan')
+_('hours', domain='Ondestan')
+_('minute', domain='Ondestan')
+_('minutes', domain='Ondestan')
+_('second', domain='Ondestan')
+_('seconds', domain='Ondestan')
 
 utc_timezone = tz.tzutc()
 default_timezone = tz.gettz(Config.get_string_value('config.default_timezone'))
@@ -47,8 +62,81 @@ def get_custom_localizer(locale):
     return localizer
 
 
+def get_fancy_time(d, display_full_version=True, request=None, locale=None):
+
+    """Returns a user friendly date format
+    d: some datetime instace in the past
+    display_second_unit: True/False
+
+    Author: Sunil Arora (http://sunilarora.org/)
+    """
+
+    if request != None:
+        localizer = get_localizer(request)
+    else:
+        localizer = get_custom_localizer(locale)
+
+    #some helpers lambda's
+    plural = lambda x: 's' if x > 1 else ''
+    singular = lambda x: x[:-1]
+    #convert pluran (years) --> to singular (year)
+    display_unit = lambda unit, name: '%s%s' %\
+        (name, plural(unit)) if unit > 0 else ''
+
+    #time units we are interested in descending order of significance
+    tm_units = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
+
+    now = datetime.utcnow()
+    now = now.replace(tzinfo=utc_timezone)
+    rdelta = relativedelta(now, d)  # capture the date difference
+    for idx, tm_unit in enumerate(tm_units):
+        first_unit_val = getattr(rdelta, tm_unit)
+        if first_unit_val > 0:
+            primary_unit = localizer.translate(_(
+                        display_unit(first_unit_val, singular(tm_unit)),
+                        domain='Ondestan'))
+            if display_full_version and idx < len(tm_units) - 1:
+                next_unit = tm_units[idx + 1]
+                second_unit_val = getattr(rdelta, next_unit)
+                if second_unit_val > 0:
+                    secondary_unit = localizer.translate(_(
+                        display_unit(second_unit_val, singular(next_unit)),
+                        domain='Ondestan'))
+                    parameters = {
+                        'primary_val': first_unit_val,
+                        'primary_unit': primary_unit,
+                        'secondary_val': second_unit_val,
+                        'secondary_unit': secondary_unit
+                    }
+                    return localizer.translate(_("fancy_time_two_units",
+                                                 domain='Ondestan',
+                                                 mapping=parameters))
+            parameters = {
+                'primary_val': first_unit_val,
+                'primary_unit': primary_unit
+            }
+            return localizer.translate(_("fancy_time_one_unit",
+                                         domain='Ondestan',
+                                         mapping=parameters))
+    return None
+
+
+def get_fancy_time_from_utc(d, display_full_version=True, request=None, locale=None):
+    return get_fancy_time(d.replace(tzinfo=utc_timezone), display_full_version, request, locale)
+
+
+def convert_to_utc(d, from_tz):
+    return d.replace(tzinfo=from_tz).\
+        astimezone(utc_timezone).replace(tzinfo=None)
+
+
+def convert_from_utc(d, to_tz):
+    return d.replace(tzinfo=utc_timezone).\
+        astimezone(to_tz)
+
+
 def format_utcdate(date, request=None, locale=None):
-    local_date = date.replace(tzinfo=utc_timezone).astimezone(default_timezone)
+    local_date = convert_from_utc(date, default_timezone)
     if request != None:
         localizer = get_localizer(request)
     else:
@@ -59,8 +147,7 @@ def format_utcdate(date, request=None, locale=None):
 
 
 def format_utcdatetime(datetime, request=None, locale=None):
-    local_datetime = datetime.replace(tzinfo=utc_timezone).astimezone(
-                                                        default_timezone)
+    local_datetime = convert_from_utc(datetime, default_timezone)
     if request != None:
         localizer = get_localizer(request)
     else:
@@ -72,7 +159,7 @@ def format_utcdatetime(datetime, request=None, locale=None):
 
 
 def format_utctime(time, request=None, locale=None):
-    local_time = time.replace(tzinfo=utc_timezone).astimezone(default_timezone)
+    local_time = convert_from_utc(time, default_timezone)
     if request != None:
         localizer = get_localizer(request)
     else:
@@ -84,6 +171,4 @@ def format_utctime(time, request=None, locale=None):
 
 def parse_to_utcdatetime(dttm):
     local_datetime = datetime.strptime(dttm, date_format)
-    utc_datetime = local_datetime.replace(tzinfo=default_timezone).\
-        astimezone(utc_timezone).replace(tzinfo=None)
-    return utc_datetime
+    return convert_to_utc(local_datetime, default_timezone)
