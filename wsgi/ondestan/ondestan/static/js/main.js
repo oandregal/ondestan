0d5816f30766beb9142fea850c686c568f9cc4e8
Jorge López <jlopez@cartolab.es>
2014-07-11 18:29:56 +0200

    Slight changes onto the navbar menu

--- wsgi/ondestan/ondestan/static/js/main.js
+++ wsgi/ondestan/ondestan/static/js/main.js
@@ -1,8 +1,11 @@
-if (contextVariables.orders_popover_content != '') {
-    $('#new_devices').popover({
-        html: true,
-        placement: 'bottom',
-        trigger: 'hover',
-        content: '<ul class="list-unstyled">' + contextVariables.orders_popover_content + '</ul>',
-    });
-}
+$(function(){
+	$(".dropdown-toggle").dropdown();
+	if (contextVariables.orders_popover_content != '') {
+	    $('#new_devices').popover({
+	        html: true,
+	        placement: 'left',
+	        trigger: 'hover',
+	        content: '<ul class="list-unstyled">' + contextVariables.orders_popover_content + '</ul>',
+	    });
+	}
+});
